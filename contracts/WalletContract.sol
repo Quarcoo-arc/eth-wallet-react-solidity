f22db69da24a4cd527c2e27360ab777dd756d5dc
Michael Quarcoo <michaelquarcoo04@gmail.com>
2023-12-25 15:18:43 +0000

    Add addFunds method to wallet contract

--- contracts/WalletContract.sol
+++ contracts/WalletContract.sol
@@ -2,5 +2,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Wallet {
+    /**
+        A special function called when a transaction
+        that doesn't specify function name to be called with.
+     */
+
+    /**
+    "External" function are part of the contract interface
+    They can therefore becalled via contracts and other transactions 
+    */
+
     receive() external payable {}
+
+    function addFunds() external payable {}
+
 }
